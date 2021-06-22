d0e882d214f395e1c4032cd943422ba651724fac
Guilherme <guilherme_devz@hotmail.com>
2021-06-22 14:00:43 -0300

    nothing important happened

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,27 +1,32 @@
+//import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 
-import {Button} from '../components/Button';
+import { Button } from '../components/Button';
+//import { AuthContext } from '../contexts/AuthContext';
+
 
 import '../styles/auth.scss'
 
 
-export function NewRoom(){
-  return(
+export function NewRoom() {
+  //const { user } = useContext(AuthContext);
+
+  return (
     <div id="page-auth">
       <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas"  />
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real.</p>
       </aside>
       <main>
         <div className="main-content">
-          <img src={logoImg} alt="Letmeask"/>
+          <img src={logoImg} alt="Letmeask" />
           <h2>Criar uma nova sala</h2>
           <form>
-            <input 
+            <input
               type="text"
               placeholder="Nome da sala"
             />
