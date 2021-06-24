7b07d058221f3caf018875749f9de7dd3b6281e0
Guilherme <guilherme_devz@hotmail.com>
2021-06-24 16:58:23 -0300

    ADD: creating the hook useRoom

---
+++ src/hooks/useRoom.ts
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+
+import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
+
+
+type FirebaseQuestion= Record<string, {
+  author: {
+    avatar: string;
+    name: string
+  }
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+  likes: Record<string,{authorId: string}>
+}>;
+
+type QuestionType = {
+  id: string;
+  author: {
+    avatar: string;
+    name: string
+  }
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+  likeCount: number;
+  likeId: string | undefined;
+}
+
+export function useRoom(roomId: string){
+  const {user} = useAuth();
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
+  const [title, setTitle] = useState('');
+ 
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+    
+    roomRef.on('value', room => {
+      const databaseRoom = room.val();
+      const roomQuestions = databaseRoom.questions as FirebaseQuestion ?? {};
+      const parsedQuestions = Object.entries(roomQuestions).map(([key, value]) => {
+        return{
+          id: key,
+          content: value.content,
+          author: value.author,
+          isAnswered: value.isAnswered,
+          isHighlighted: value.isHighlighted,
+          likeCount: Object.values(value.likes ?? {}).length,
+          likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0]
+        }
+      })
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    })
+
+    return () => { roomRef.off('value')}
+  }, [roomId, user?.id]);
+
+  return {questions, title}
+}
